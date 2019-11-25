f8e350f3a5ca1fdcd7b43640263f6a159c26012d
Adelson <adelsonagostinhons@hotmail.com>
2019-11-25 01:41:06 -0300

    pequenas mudanças frontend

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React from 'react';
 import './App.css';
 import api from './api.js';
 
-api.get("/quizzes");
+
 
 function App() {
+  
+
+ async function handlePost(){
+
+   const quiz = await api.get('/quizzes');
+   const dados = quiz.data;
+   console.log(dados);
+
+ }
+ handlePost();
+
+
+
   return (
-    <div className="App">
+    <div  className="App">
+      <div className="compApp">
+          <button className="Cvoltar">Voltar</button> 
+          <img/>
+            <div className="compQuiz">
+              <h1>Titulo Quiz</h1>
+              
+             <p>
+                Descrição asdasda
+             </p>   
+             <form>
+                <span>Pergunta :</span>                
+                <input/><br></br>
+                <span>Pergunta :</span>                
+                <input/>
+             </form>
+
+             <button className="btn">Enviar</button>
+
+            </div>
+      </div>
       Hello World!
+      
     </div>
   );
 }
